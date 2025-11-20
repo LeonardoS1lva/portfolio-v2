a54f382b0933da83fe6b5738ceb6a8399ad5b813
Leonardo Silva <donar_leo@hotmail.com>
2025-11-20 12:21:59 -0300

    feat(ProjectList): adicionar componente CardListProjects para exibir projetos

---
+++ src/components/cards/CardListProjects.vue
@@ -0,0 +1,141 @@
+<script setup>
+import defaultImg from 'src/assets/projects/PlaceholderImg.png'
+import { computed } from 'vue'
+
+const props = defineProps({
+  imgProject: {
+    type: String,
+    default: defaultImg,
+  },
+  altText: {
+    type: String,
+    default: 'Project Image',
+  },
+  projectName: {
+    type: String,
+    default: 'Projeto Exemplo',
+  },
+  projectDescription: {
+    type: String,
+    default: 'Descrição breve do projeto exemplo.',
+  },
+  colorStatus: {
+    type: String,
+    default: 'primary-color',
+  },
+  textColorStatus: {
+    type: String,
+    default: 'primary-color',
+  },
+  status: {
+    type: Number,
+    default: 4,
+  },
+  hasLinkCode: {
+    type: Boolean,
+    default: true,
+  },
+  hasLinkProject: {
+    type: Boolean,
+    default: true,
+  },
+  linkCode: {
+    type: String,
+  },
+  linkProject: {
+    type: String,
+  },
+  stacks: {
+    type: Array,
+    default: () => [],
+  },
+})
+
+const getStatus = computed(() => {
+  switch (props.status) {
+    case 1:
+      return {
+        status: 'Em progresso',
+        backgroundColor: 'yellow-3',
+        textColor: 'yellow-10',
+      }
+    case 2:
+      return {
+        status: 'Concluído',
+        backgroundColor: 'green-3',
+        textColor: 'green-10',
+      }
+    case 3:
+      return {
+        status: 'Pausado',
+        backgroundColor: 'red-3',
+        textColor: 'red-10',
+      }
+    default:
+      return {
+        status: 'Desconhecido',
+        backgroundColor: 'grey-3',
+        textColor: 'grey-10',
+      }
+  }
+})
+</script>
+
+<template>
+  <q-card
+    style="width: 100%; max-width: 400px; border-radius: 12px"
+    class="row column full-height bg-background-card"
+  >
+    <q-img :src="imgProject" :alt="altText" :ratio="16 / 9" />
+
+    <q-card-section>
+      <div class="text-h6 text-secondary-color row justify-between no-wrap items-center">
+        <span>{{ projectName }}</span>
+
+        <q-chip
+          outline
+          size="sm"
+          :color="getStatus.textColor"
+          :text-color="getStatus.textColor"
+          :class="`bg-${getStatus.backgroundColor} text-bold`"
+        >
+          {{ getStatus.status }}
+        </q-chip>
+      </div>
+      <div class="text-body2 text-secondary-color-light q-pt-sm">{{ projectDescription }}</div>
+    </q-card-section>
+
+    <q-card-section v-if="stacks.length">
+      <q-chip v-for="stack in stacks" :key="stack" color="primary-color" text-color="third-color">
+        {{ stack }}
+      </q-chip>
+    </q-card-section>
+
+    <q-space />
+
+    <q-card-actions align="right">
+      <q-btn
+        flat
+        no-caps
+        color="primary-color"
+        text-color="secondary-color"
+        label="Ver Código"
+        :href="linkCode"
+        target="_blank"
+        rel="noopener noreferrer"
+        v-if="hasLinkCode"
+      />
+      <q-btn
+        flat
+        no-caps
+        color="primary-color"
+        text-color="secondary-color"
+        label="Ver Projeto"
+        :href="linkProject"
+        target="_blank"
+        rel="noopener noreferrer"
+        v-if="hasLinkProject"
+      />
+    </q-card-actions>
+  </q-card>
+</template>
--- src/pages/ProjectList.vue
+++ src/pages/ProjectList.vue
@@ -1,32 +1,74 @@
 <script setup>
-import PixelButton2 from 'src/components/buttons/PixelButton2.vue'
+import { computed, ref } from 'vue'
+import CardListProjects from 'src/components/cards/CardListProjects.vue'
+import projects from 'src/utils/allProjects'
+
+const filters = ['Todos', 'Vue/Quasar', 'HTML/CSS', 'Javascript', 'Outros']
+const activeFilter = ref('Todos')
+
+const filteredItems = computed(() => {
+  if (activeFilter.value === 'Todos') {
+    return projects
+  } else if (activeFilter.value === 'Vue/Quasar') {
+    return projects.filter(
+      (project) => project.stacks.includes('Vue') || project.stacks.includes('Quasar'),
+    )
+  } else if (activeFilter.value === 'HTML/CSS') {
+    return projects.filter(
+      (project) => project.stacks.includes('HTML') || project.stacks.includes('CSS'),
+    )
+  } else if (activeFilter.value === 'Javascript') {
+    return projects.filter((project) => project.stacks.includes('JavaScript'))
+  }
+  return projects.filter((project) =>
+    project.stacks.some((stack) => !['Vue', 'Quasar', 'HTML', 'CSS', 'JavaScript'].includes(stack)),
+  )
+})
 </script>
 
 <template>
-  <q-page class="flex flex-center">
-    <!-- <q-card class="text-center bg-background-card" flat style="max-width: 450px; width: 100%">
+  <q-page class="row">
+    <q-space />
+    <div class="q-pt-xl col-11">
       <h1 class="text-h3 text-secondary-color">Lista de Projetos</h1>
-      <h2 class="text-h6 q-pt-md text-secondary-color-light">
+      <h2 class="text-h6 q-pt-sm text-secondary-color-light">
         Aqui estão alguns dos meus projetos mais recentes.
       </h2>
-      <q-btn
-        color="primary-color"
-        text-color="third-color"
-        label="Voltar para o Início"
-        to="/"
-        class="q-mt-md"
-      />
-    </q-card> -->
-    <PixelButton2 />
+
+      <div class="q-py-md row q-gutter-md">
+        <q-btn
+          v-for="filter in filters"
+          :key="filter"
+          :label="filter"
+          :color="activeFilter === filter ? 'primary-color' : 'grey-6'"
+          text-color="third-color"
+          outlined
+          rounded
+          @click="activeFilter = filter"
+        />
+      </div>
+
+      <div class="q-pt-md q-pb-lg row q-col-gutter-md">
+        <div
+          v-for="project in filteredItems"
+          :key="project"
+          class="col-lg-3 col-md-4 col-sm-6 col-12 row justify-center"
+        >
+          <CardListProjects
+            :imgProject="project.imgProject"
+            :altText="project.altText"
+            :projectName="project.projectName"
+            :projectDescription="project.projectDescription"
+            :status="project.status"
+            :hasLinkCode="project.hasLinkCode"
+            :hasLinkProject="project.hasLinkProject"
+            :linkCode="project.codeLink"
+            :linkProject="project.projectLink"
+            :stacks="project.stacks"
+          />
+        </div>
+      </div>
+    </div>
+    <q-space />
   </q-page>
 </template>
-
-<style scoped>
-/* .pixel-border {
-  border-width: 32px;
-  border-style: solid;
-  border-image: url('/src/assets/icons/pixel-art/border_square_pxArt.png') 4 stretch;
-  image-rendering: pixelated;
-  background: transparent;
-} */
-</style>
