84a860d07e31e86b68896368fc09941a9dce0b0f
Leonardo Silva <donar_leo@hotmail.com>
2025-11-20 12:22:06 -0300

    feat(allProjects): adicionar arquivo allProjects.js com informações dos projetos

---
+++ src/utils/allProjects.js
@@ -0,0 +1,47 @@
+// status: 1 = Em progresso, 2 = Concluído, 3 = Pausado
+
+export default [
+  {
+    imgProject: 'src/assets/projects/plataforma_cpid.webp',
+    projectName: 'Plataforma CPID',
+    projectDescription:
+      'A Plataforma é um sistema desenvolvido para apresentação e organização dos projetos do laboratório de Cidades Sustentáveis localizado no CPID.',
+    projectLink: 'https://plataformacpid.com.br/',
+    labelButton: 'Ver Projeto',
+    hasLinkCode: false,
+    status: 1,
+    stacks: ['Vue', 'Quasar', 'Python', 'MySQL'],
+  },
+  {
+    imgProject: 'src/assets/projects/projeto_roda_da_vida.webp',
+    projectName: 'Roda da Vida',
+    projectDescription:
+      'Ferramenta de autoconhecimento que ajuda a medir o nível de satisfação em diferentes aspectos da , focado em mulheres com jornada dupla de trabalho.',
+    projectLink: 'https://projeto-roda-da-vida.vercel.app/',
+    labelButton: 'Ver Projeto',
+    hasLinkCode: false,
+    status: 2,
+    stacks: ['HTML', 'CSS', 'JavaScript'],
+  },
+  {
+    imgProject: 'src/assets/projects/beija_flor.webp',
+    projectName: 'Rede Beija Flor',
+    projectDescription:
+      'Um site para organização de dados coletados por um sistema de monitoramento ambiental de beija-flores.',
+    projectLink: 'https://app1.arandu.org.br/',
+    labelButton: 'Ver Projeto',
+    hasLinkCode: false,
+    status: 1,
+    stacks: ['Vue', 'Quasar'],
+  },
+  {
+    imgProject: 'src/assets/projects/jogo_da_memoria.webp',
+    projectName: 'Jogo da Memória',
+    projectDescription:
+      'Um jogo da memória simples desenvolvido para ajudar crianças a melhorar suas habilidades cognitivas e de memória.',
+    projectLink: 'https://jogo-da-memoria-2-nu.vercel.app/',
+    labelButton: 'Ver Projeto',
+    status: 2,
+    stacks: ['Vue', 'Quasar'],
+  },
+]
