e3609d1c91bfc6a7b240c827ba6a8c3be6eb763d
Leonardo Silva <donar_leo@hotmail.com>
2025-11-21 14:25:24 -0300

    refactor(DefaultLayout): integrar cabeçalho e rodapé pixel-art com base no tema

--- src/layouts/DefaultLayout.vue
+++ src/layouts/DefaultLayout.vue
@@ -1,16 +1,25 @@
 <script setup>
 import TheFooter from 'src/components/default/TheFooter.vue'
 import TheHeader from 'src/components/default/TheHeader.vue'
+
+import TheHeaderPixel from 'src/components/pixelArt/TheHeaderPixel.vue'
+import TheFooterPixel from 'src/components/pixelArt/TheFooterPixel.vue'
+
+import { useThemeStore } from 'src/stores/themeStore'
+
+const useTheme = useThemeStore()
 </script>
 
 <template>
   <q-layout view="lHh Lpr lff">
-    <TheHeader :show-links="false" />
+    <TheHeaderPixel v-if="useTheme.theme === 'pixel-art'" :show-links="false" />
+    <TheHeader v-else :show-links="false" />
 
     <q-page-container>
       <router-view />
     </q-page-container>
 
-    <TheFooter :show-contact="false" />
+    <TheFooterPixel v-if="useTheme.theme === 'pixel-art'" :show-contact="false" />
+    <TheFooter v-else :show-contact="false" />
   </q-layout>
 </template>
